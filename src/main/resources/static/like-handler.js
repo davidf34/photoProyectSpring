e8d7813d80151e3dea7417cc4860451dad793630
David Sandoval <david.sandoval-g@mail.escuelaing.edu.co>
2025-06-26 15:09:54 -0500

    Modificacion de logica en JS

--- src/main/resources/static/like-handler.js
+++ src/main/resources/static/like-handler.js
@@ -8,11 +8,11 @@ document.addEventListener("DOMContentLoaded", function () {
             const photoId = this.getAttribute("data-id");
 
             fetch(`/like/${photoId}`)
-                .then(response => response.json())
-                .then(newLikes => {
+                .then(response => response.json()) //se convierte el json del back a un objeto de JS, tipo JSON
+                .then(newLikes => { //acá se obtiene el valor del JSON anterior es decir zSi el backend devolvió { newLikes: 5 }, newLikes será 5.
                     if (newLikes !== -1) {
                         const likeSpan = document.getElementById(`likes-${photoId}`);
-                        likeSpan.textContent = newLikes;
+                        likeSpan.textContent = newLikes; //.textContent: Es la propiedad que asigna el nuevo valor al contenido de texto del <span>
                     } else {
                         alert("Error al dar like.");
                     }
